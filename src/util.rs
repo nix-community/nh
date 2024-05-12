05740e9f18fa7ccbca26e62211931bc4994cade5
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-05-12 17:25:23 -0300

    Make the get_elevation_program function more consise

--- src/util.rs
+++ src/util.rs
@@ -291,20 +291,13 @@ pub fn print_dix_diff(old_generation: &Path, new_generation: &Path) -> Result<()
 /// * `Result<OsString>` - The absolute path to the privilege elevation program binary or an error if a
 /// program can't be found.
 pub fn get_elevation_program() -> Result<OsString> {
-    let has_doas = which("doas");
-    if let Ok(path) = has_doas {
-        debug!(?path, "doas path found");
-        return Ok(path.into_os_string());
-    }
-    let has_sudo = which("sudo");
-    if let Ok(path) = has_sudo {
-        debug!(?path, "sudo path found");
-        return Ok(path.into_os_string());
-    }
-    let has_pkexec = which("pkexec");
-    if let Ok(path) = has_pkexec {
-        debug!(?path, "pkexec path found");
-        return Ok(path.into_os_string());
+    let strategies = ["doas", "sudo", "pkexec"];
+
+    for strategy in strategies {
+        if let Ok(path) = which(strategy) {
+            debug!(?path, "{strategy} path found");
+            return Ok(path.into_os_string());
+        }
     }
 
     Err(eyre::eyre!("No elevation strategy found"))
