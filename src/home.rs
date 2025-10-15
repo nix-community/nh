407d6c474249d3c629eb714c988c8659a220a7fa
faukah <fau@faukah.com>
2025-10-15 23:56:58 +0200

    installable: make installable attribute be a String
    There is no point in making the attribute of an
    Installable be a Vec. Attributes can are used in one of:
    - Flake: FLAKEREF#ATTRIBUTE:
      Here, we cannot pass multiple attributes anyways.
    
    - File, Expression:
      The Nix expression (in that file), or any selected attribute,
      must evaluate to a derivation.
    
    It is fine to only accept a single attribute here,
    since no one is going to add several attributes corresponding
    to {nixos,home,darwin}Configurations, here.

--- src/home.rs
+++ src/home.rs
@@ -69,10 +69,7 @@ impl HomeRebuildArgs {
         Some(r) => r.to_owned(),
         None => return Err(eyre!("NH_HOME_FLAKE missing reference part")),
       };
-      let attribute = elems
-        .next()
-        .map(crate::installable::parse_attribute)
-        .unwrap_or_default();
+      let attribute = elems.next().unwrap_or_default().to_string();
 
       Installable::Flake {
         reference,
@@ -223,7 +220,7 @@ where
         return Ok(res);
       }
 
-      attribute.push(String::from("homeConfigurations"));
+      *attribute = String::from("homeConfigurations");
 
       let flake_reference = reference.clone();
       let mut found_config = false;
@@ -254,7 +251,7 @@ where
         if check_res.map(|s| s.trim().to_owned()).as_deref() == Some("true") {
           debug!("Using explicit configuration from flag: {config_name:?}");
 
-          attribute.push(config_name);
+          attribute.push_str(&config_name);
           if push_drv {
             attribute.extend(toplevel.clone());
           }
@@ -264,7 +261,7 @@ where
           // Explicit config provided but not found
           let tried_attr_path = {
             let mut attr_path = attribute.clone();
-            attr_path.push(config_name);
+            attr_path.push_str(&config_name);
             Installable::Flake {
               reference: flake_reference,
               attribute: attr_path,
@@ -309,7 +306,7 @@ where
 
           let current_try_attr = {
             let mut attr_path = attribute.clone();
-            attr_path.push(attr_name.clone());
+            attr_path.push_str(&attr_name);
             attr_path
           };
           tried.push(current_try_attr.clone());
@@ -318,7 +315,7 @@ where
             check_res.map(|s| s.trim().to_owned()).as_deref()
           {
             debug!("Using automatically detected configuration: {}", attr_name);
-            attribute.push(attr_name);
+            attribute.push_str(&attr_name);
             if push_drv {
               attribute.extend(toplevel.clone());
             }
@@ -379,10 +376,7 @@ impl HomeReplArgs {
         Some(r) => r.to_owned(),
         None => return Err(eyre!("NH_HOME_FLAKE missing reference part")),
       };
-      let attribute = elems
-        .next()
-        .map(crate::installable::parse_attribute)
-        .unwrap_or_default();
+      let attribute = elems.next().unwrap_or_default().to_string();
 
       Installable::Flake {
         reference,
