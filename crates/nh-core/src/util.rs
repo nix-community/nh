0cf53644ff4c90490bc3abcd3e6258ac18d28047
faukah <fau@faukah.com>
2026-02-28 22:38:22 +0100

    nh-core/util: fix tests

--- crates/nh-core/src/util.rs
+++ crates/nh-core/src/util.rs
@@ -342,10 +342,10 @@ pub fn get_missing_experimental_features(
 /// # Examples
 ///
 /// ```rust
-/// use nh_command::ElevationStrategy;
+/// use nh_core::command::ElevationStrategy;
 ///
 /// // Elevate the current process to run as root
-/// let elevate: fn(ElevationStrategy) -> ! = nh_util::self_elevate;
+/// let elevate: fn(ElevationStrategy) -> ! = nh_core::util::self_elevate;
 /// ```
 #[allow(clippy::panic, clippy::expect_used)]
 pub fn self_elevate(strategy: ElevationStrategy) -> ! {
