d4cb2598653125e63e423153d1c253e944d2fcb6
Amaan Qureshi <git@amaanq.com>
2026-06-06 04:51:58 -0400

    flake: build for all nixpkgs systems

--- flake.nix
+++ flake.nix
@@ -7,13 +7,21 @@
       nixpkgs,
     }:
     let
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          "aarch64-darwin"
-        ] (system: function nixpkgs.legacyPackages.${system});
+      inherit (nixpkgs) lib;
+
+      pkgsFor = system: nixpkgs.legacyPackages.${system} or (import nixpkgs { inherit system; });
+
+      supportedSystems = builtins.filter (
+        system: (builtins.tryEval (pkgsFor system).stdenv.hostPlatform).success
+      ) (lib.systems.doubles.linux ++ lib.systems.doubles.darwin);
+
+      forAllSystems = function: lib.genAttrs supportedSystems (system: function (pkgsFor system));
+
+      ciSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
       rev = self.shortRev or self.dirtyShortRev or "dirty";
     in
@@ -25,7 +33,7 @@
         default = self.packages.${pkgs.stdenv.hostPlatform.system}.nh;
       });
 
-      checks = self.packages // self.devShells;
+      checks = lib.genAttrs ciSystems (system: self.packages.${system});
 
       devShells = forAllSystems (pkgs: {
         default = import ./shell.nix { inherit pkgs; };
